d51431887a350cbc56e60881384cc8bafbf23faf
RCH <radovan.chytracek@rcconsult.biz>
2019-02-18 20:40:55 +0100

    Fixing regression after adding basic dashboard. Fixing the CSS display problem. Fixing the CSS wrong font attributes.

--- js/index.js
+++ js/index.js
@@ -1,33 +1,72 @@
-var clickedData = "";
-var clickedButton = "";
-var clicked_letters = "";
-var penalize_same_letter = false;
-var lives = 9;
-var all_is_guessed = false;
-var strategy = "linear"; /* linear, boottomup, headfirst */
+let clickedData = "";
+let clickedButton = "";
+let clicked_letters = "";
+let penalize_same_letter = true;
+let lives = 9;
+let all_is_guessed = false;
+let game_is_over = false;
+let strategy = "bottomup"; /* linear, boottomup, headfirst */
 
-var hangProgress = {
+let hangProgress = {
     lives_rest: 9,
     index: 0,
     linear: ["h11", "h12", "h13", "h21", "h22", "h23", "h31", "h32", "h33"],
-    boottomup: ["h31", "h21", "h11", "h12", "h13", "h22", "h23", "h32", "h33"],
+    bottomup: ["h31", "h21", "h11", "h12", "h13", "h22", "h23", "h32", "h33"],
     headfirst: ["h12", "h22", "h32", "h23", "h33", "h13", "h11", "h21", "h31"]
 };
 
+/* Timing */
+let sec = 0;
+
+function pad(val) { return val > 9 ? val : "0" + val; }
+setInterval(function() {
+    let label = pad(parseInt(sec / 60, 10)) + ":" + pad(++sec % 60);
+    document.getElementById("timing").innerText = label;
+}, 1000);
+
+function handle_form_reset(form) {
+    game_is_over = true;
+    let tile_lives = document.getElementById("infomsg");
+    tile_lives.innerText = "YOU LOST! GAME OVER!!!";
+}
+
+function validate(form) {
+    if (hangProgress.lives_rest === 0 && !all_is_guessed && !game_is_over) {
+        form.reset();
+    }
+}
+
+function render_lives() {
+    let tile_lives = document.getElementById("lives");
+    tile_lives.innerText = parseInt(hangProgress.lives_rest);
+}
+
 function render_hungman() {
-    if (hangProgress.index >= 0 && hangProgress.index < 9) {
-        var tile_img = document.getElementById(hangProgress[strategy][hangProgress.index]);
-        tile_img.style.display = "block";
+    render_lives();
+
+    if (hangProgress.index >= 1 && hangProgress.index <= 9) {
+        let tile_img = document.getElementById(hangProgress[strategy][hangProgress.index - 1]);
+        //tile_img.style.display = "block";
+        tile_img.style.visibility = "visible";
     }
 }
 
 function take_life() {
-    hangProgress.lives_rest = hangProgress.lives_rest - 1;
-    hangProgress.index = 9 - hangProgress.lives_rest;
+    if (hangProgress.lives_rest > 0) {
+        hangProgress.lives_rest = hangProgress.lives_rest - 1;
+        hangProgress.index = 9 - hangProgress.lives_rest;
+    }
 }
 
-function validate(form) {
-    var clicked = document.getElementById("clicked_letters");
+function clickedme(button) {
+    if (game_is_over) {
+        return false;
+    }
+    clickedData = button.value;
+    clickedButton = button.id;
+    console.log("Clicked: " + String(button.id) + " data: " + String(clickedData));
+
+    let clicked = document.getElementById("clicked_letters");
     if (clickedData == ' ') {
         clickedData = '\u2423';
     }
@@ -38,42 +77,30 @@ function validate(form) {
         console.log("Update: " + clicked_letters);
         if (guess_right(clickedData)) {
             /* We guessed the letter right so we update the clicked letters */
-            1 === 1;
+            console.log("TODO: Update function after the correct guess");
         } else {
-            render_hungman();
             take_life();
-            if (hangProgress.lives_rest === 0 && !all_is_guessed) {
-                alert("LOST! GAME OVER!!!");
-            }
+            render_hungman();
         }
     } else {
         if (penalize_same_letter) {
-            render_hungman();
             take_life();
-            if (hangProgress.lives_rest === 0 && !all_is_guessed) {
-                alert("LOST! GAME OVER!!!");
-            }
+            render_hungman();
         }
     }
-    return true;
-}
 
-function clickedme(button) {
-    clickedData = button.value;
-    clickedButton = button.id;
-    console.log("Clicked: " + String(button.id) + " data: " + String(clickedData));
     return true;
 }
 
 function init_hangman() {
-    var buttonsList = document.getElementById("alphabet");
+    let buttonsList = document.getElementById("alphabet");
 
-    var abcd = "abcdefghijklmnopqrstuvwxyz ";
-    var i = 0;
-    var newletc = "";
+    let abcd = "abcdefghijklmnopqrstuvwxyz ";
+    let i = 0;
+    let newletc = "";
     while (i < abcd.length) {
-        var buttonLabel = String(abcd.charAt(i));
-        var buttonId = "id" + String(abcd.charAt(i));
+        let buttonLabel = String(abcd.charAt(i));
+        let buttonId = "id" + String(abcd.charAt(i));
         if (abcd.charAt(i) == ' ') {
             buttonLabel = '\u2423';
             buttonId = "spacebar";
@@ -90,6 +117,8 @@ function init_hangman() {
     // console.log(newletc);
     buttonsList.innerHTML = newletc;
 
+    render_lives();
+
     console.log("HANGMAN STARTED");
 }
 
@@ -97,20 +126,34 @@ function guess_right() {
     return false;
 }
 
+function game_options() {
+    console.log("TODO: Game options panel");
+}
+
 function new_game() {
     reset_game();
     location.reload();
 }
 
-function game_options() {;
+function hide_hangman_tiles() {
+    let tile_div = document.getElementById("hangman").childNodes;
+    let count = tile_div.length;
+    let i = 0;
+    while (i < 9) {
+        document.getElementById(hangProgress[strategy][i]).style.visibility = "hidden";
+        i += 1;
+    }
 }
 
 function reset_game() {
     hangProgress.index = 0;
     hangProgress.lives_rest = 9;
     clicked_letters = "";
-    var clicked = document.getElementById("clicked_letters");
+    let clicked = document.getElementById("clicked_letters");
     clicked.innerText = clicked_letters;
+    hide_hangman_tiles();
+    render_lives();
+    game_is_over = false;
 }
 
 function panelclick(button) {
